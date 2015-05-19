78ca11fd9988573c9b05930e3dc497660c83fc73
taiti <taiti@stone-rise.com>
2015-05-19 22:54:14 +0900

    chapter15 - 10

--- greet.php
+++ greet.php
@@ -1,3 +1,10 @@
 <?php
-	echo htmlspecialchars("hi!". $_GET['name'], ENT_QUOTES, "utf-8");
+	// echo htmlspecialchars("hi! ". $_GET['name'], ENT_QUOTES, "utf-8");
+	$rs = array(
+		"message" => htmlspecialchars("hi! ". $_GET['name'], ENT_QUOTES, "utf-8"),
+		"length" => strien($_GET['name'])
+	);
+
+	header('Content-Type: application/json; charset=utf-8');
+	echo json_endoe($rs);
 ?>
